aae9a3a1c1f1ce09f59274f97e53f54a26e68754
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2021-04-08 23:24:03 -0300

    Pequenas melhorias de formatação na classe Wallet.

--- lib/domain/models/wallet.dart
+++ lib/domain/models/wallet.dart
@@ -1,20 +1,43 @@
 import 'dart:collection';
+import 'package:king_investor/domain/models/asset.dart';
 import 'package:king_investor/shared/models/model.dart';
 
 class Wallet extends Model {
-  bool isMainWallet;
-  String name;
-  List<WalletItem> _items;
+  bool _isMainWallet;
+  String _name;
+  List<Asset> _assets;
 
-  Wallet(String uid, this.isMainWallet, this.name) : super(uid) {
-    _items = <WalletItem>[];
+  Wallet(String objectId, DateTime createdAt, bool isMainWallet, String name) : super(objectId, createdAt) {
+    _name = name ?? 'Carteira ${DateTime.now().millisecondsSinceEpoch / 1000}';
+    _isMainWallet = isMainWallet ?? false;
+    _assets = <Asset>[];
   }
 
-  Wallet.createMainWallet() : super(null) {
-    this.isMainWallet = true;
-    this.name = 'Principal';
-    _items = <WalletItem>[];
+  Wallet.createMainWallet() : super(null, null) {
+    _isMainWallet = true;
+    _name = 'Principal';
+    _assets = <Asset>[];
   }
 
-  UnmodifiableListView<WalletItem> get items => UnmodifiableListView<WalletItem>(_items);
+  bool get isMainWallet => _isMainWallet;
+  String get name => _name;
+  UnmodifiableListView<Asset> get items => UnmodifiableListView<Asset>(_assets);
+
+  void setName(String name) {
+    if (name != null && name.trim() != '') _name = name;
+  }
+
+  void addAsset(Asset asset) {
+    clearNotifications();
+    if (asset == null) addNotification('Wallet.assets', 'O item adicionado não pode ser "null"');
+    if (_assets.contains(asset)) addNotification('Wallet.assets', 'Não é possivel adicionar itens duplicados');
+    if (isValid) _assets.add(asset);
+  }
+
+  void removeAsset(Asset asset) {
+    clearNotifications();
+    if (asset == null) addNotification('Wallet.assets', '"Null" não é um valor válido para esta lista');
+    if (!_assets.contains(asset)) addNotification('Wallet.assets', 'O item não existe na lista');
+    if (isValid) _assets.remove(asset);
+  }
 }
