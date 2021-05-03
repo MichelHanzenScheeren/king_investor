2b1b9c09c4810e91374d9ed218dc497c3c231df8
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2021-05-03 20:43:35 -0300

    Melhorias de organização na classe WalletsUseCase

--- lib/domain/use_cases/wallets_use_case.dart
+++ lib/domain/use_cases/wallets_use_case.dart
@@ -18,83 +18,130 @@ class WalletsUseCase {
     if (_appData.currentUserId.isEmpty) return Left(Notification('WalletsUseCase.getWallets', 'Usuário indisponível'));
     if (_appData.wallets.isNotEmpty) return Right(_appData.wallets);
     final response = await _database.filterByRelation(Wallet, [User], [_appData.currentUserId]);
-    return response.fold((notification) => Left(notification), (resultList) async {
-      final List<Wallet> newWallets = List<Wallet>.from(resultList);
-      if (newWallets.isEmpty) return await _createAndRegisterMainWallet();
-      _appData.registerWallets(newWallets);
-      return Right(_appData.wallets);
-    });
+    return response.fold(
+      (notification) => Left(notification),
+      (resultList) async {
+        final List<Wallet> newWallets = List<Wallet>.from(resultList);
+        if (newWallets.isEmpty) return await _createAndRegisterMainWallet();
+        _appData.registerWallets(newWallets);
+        return Right(_appData.wallets);
+      },
+    );
   }
 
   Future<Either<Notification, List<Wallet>>> _createAndRegisterMainWallet() async {
     final Wallet mainWallet = Wallet.createMainWallet(_appData.currentUserId);
     final response = await _database.create(mainWallet);
-    return response.fold((notification) => Left(notification), (id) {
-      mainWallet.setObjectId(id);
-      _appData.registerWallets([mainWallet]);
-      return Right(_appData.wallets);
-    });
+    return response.fold(
+      (notification) => Left(notification),
+      (id) {
+        mainWallet.setObjectId(id);
+        _appData.registerWallets([mainWallet]);
+        return Right(_appData.wallets);
+      },
+    );
   }
 
   Future<Either<Notification, String>> addWallet(Wallet newWallet) async {
-    if (newWallet == null || !newWallet.isValid)
-      return Left(Notification('WalletsUseCase.addWallet', 'Uma carteira inválida não pode ser salva'));
-    if (_appData.hasWallet(newWallet.objectId))
-      return Left(Notification('WalletsUseCase.addWallet', 'Não é possível adicionar uma carteira duplicada'));
-    if (_appData.duplicatedMainWallet(newWallet))
-      return Left(Notification('WalletsUseCase.addWallet', 'Não é possível registrar duas carteiras principais'));
+    final validate = _validateWalletToAdd(newWallet);
+    if (validate.isLeft()) return validate;
     final response = await _database.create(newWallet);
-    return response.fold((notification) => Left(notification), (objectId) {
-      newWallet.setObjectId(objectId);
-      _appData.registerWallets([newWallet]);
-      return Right(objectId);
-    });
+    return response.fold(
+      (notification) => Left(notification),
+      (objectId) {
+        newWallet.setObjectId(objectId);
+        _appData.registerWallets([newWallet]);
+        return Right(objectId);
+      },
+    );
   }
 
   Future<Either<Notification, Notification>> deleteWallet(String walletId) async {
-    if (!_appData.hasWallet(walletId))
-      return Left(Notification('WalletsUseCase.deleteWallet', 'Carteira não encontrada'));
+    final validate = _validateWalletToDelete(walletId);
+    if (validate.isLeft()) return validate;
     final Wallet wallet = _appData.getWalletById(walletId);
-    if (wallet.isMainWallet)
-      return Left(Notification('WalletsUseCase.deleteWallet', 'Não é possível apagar a carteira principal'));
     final response = await _database.delete(wallet);
-    return response.fold((notification1) => Left(notification1), (notification2) {
-      _appData.deleteWallet(wallet);
-      return Right(notification2);
-    });
+    return response.fold(
+      (notification1) => Left(notification1),
+      (notification2) {
+        _appData.deleteWallet(wallet);
+        return Right(notification2);
+      },
+    );
   }
 
   Future<Either<Notification, Notification>> updateWallet(Wallet wallet) async {
+    final validate = _validateWalletToUpdate(wallet);
+    if (validate.isLeft()) return validate;
+    final response = await _database.update(wallet);
+    return response.fold(
+      (notification1) => Left(notification1),
+      (notification2) {
+        _appData.updateWallet(wallet);
+        return Right(notification2);
+      },
+    );
+  }
+
+  Future<Either<Notification, Notification>> changeMainWallet(String newMainWalletId) async {
+    final validate = _validateWalletsToChangeMainWallet(newMainWalletId);
+    if (validate.isLeft()) return validate;
+    final validate2 = await _setCurrentMainWallet();
+    if (validate2.isLeft()) return validate2;
+
+    final Wallet newMainWallet = _appData.getWalletById(newMainWalletId);
+    newMainWallet.setMainWallet(true);
+    final response2 = await _database.update(newMainWallet);
+    return response2.fold(
+      (notification1) => Left(notification1),
+      (notification2) => Left(notification2),
+    );
+  }
+
+  Either<Notification, String> _validateWalletToAdd(Wallet wallet) {
+    if (wallet == null || !wallet.isValid)
+      return Left(Notification('WalletsUseCase.addWallet', 'Uma carteira inválida não pode ser salva'));
+    if (_appData.hasWallet(wallet.objectId))
+      return Left(Notification('WalletsUseCase.addWallet', 'Não é possível adicionar uma carteira duplicada'));
+    if (_appData.duplicatedMainWallet(wallet))
+      return Left(Notification('WalletsUseCase.addWallet', 'Não é possível registrar duas carteiras principais'));
+    return Right('');
+  }
+
+  Either<Notification, Notification> _validateWalletToDelete(String walletId) {
+    if (!_appData.hasWallet(walletId))
+      return Left(Notification('WalletsUseCase.deleteWallet', 'Carteira não encontrada'));
+    if (_appData.getWalletById(walletId).isMainWallet)
+      return Left(Notification('WalletsUseCase.deleteWallet', 'Não é possível apagar a carteira principal'));
+    return Right(Notification('', ''));
+  }
+
+  _validateWalletToUpdate(Wallet wallet) {
     if (wallet == null || !wallet.isValid)
       return Left(Notification('WalletsUseCase.editWallet', 'Uma carteira inválida não pode ser editada'));
     if (!_appData.hasWallet(wallet.objectId))
       return Left(Notification('WalletsUseCase.editWallet', 'Carteira não encontrada'));
-    final Wallet originalWallet = _appData.getWalletById(wallet?.objectId);
+    final Wallet originalWallet = _appData.getWalletById(wallet.objectId);
     if (wallet.isMainWallet != originalWallet.isMainWallet)
-      return Left(Notification('WalletsUseCase.editWallet', 'Não é possível editar esta carteira'));
-    final response = await _database.update(wallet);
-    return response.fold((notification1) => Left(notification1), (notification2) {
-      _appData.updateWallet(wallet);
-      return Right(notification2);
-    });
+      return Left(Notification('WalletsUseCase.editWallet', 'Não é possível alterar a carteira aqui'));
+    return Right(Notification('', ''));
   }
 
-  Future<Either<Notification, Notification>> changeMainWallet(String newMainWalletId) async {
+  Either<Notification, Notification> _validateWalletsToChangeMainWallet(String newMainWalletId) {
     if (!_appData.hasWallet(newMainWalletId))
       return Left(Notification('WalletsUseCase.changeMainWallet', 'Carteira não encontrada'));
-    final Wallet newMainWallet = _appData.getWalletById(newMainWalletId);
+    if (_appData.getMainWallet()?.objectId != newMainWalletId)
+      return Left(Notification('WalletsUseCase.changeMainWallet', 'Esta já é a carteira principal'));
+    return Right(Notification('', ''));
+  }
+
+  Future<Either<Notification, Notification>> _setCurrentMainWallet() async {
     final Wallet oldMainWallet = _appData.getMainWallet();
     if (oldMainWallet != null) {
       oldMainWallet.setMainWallet(false);
       final response1 = await _database.update(oldMainWallet);
       if (response1.isLeft()) return response1;
-      _appData.updateWallet(oldMainWallet);
     }
-    newMainWallet.setMainWallet(true);
-    final response2 = await _database.update(newMainWallet);
-    return response2.fold((notification1) => Left(notification1), (notification2) {
-      _appData.updateWallet(newMainWallet);
-      return Left(notification2);
-    });
+    return Right(Notification('', ''));
   }
 }
