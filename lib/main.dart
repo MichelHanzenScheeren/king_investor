eac86c0ffb3f2a5483710d92dc25d6d174b72556
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2021-05-31 22:01:59 -0300

    Remoção da lógica para obter todas as rotas para o arquivo estático AppRoutes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:king_investor/dependencies_injection.dart';
-import 'package:king_investor/presentation/pages/asset/asset_page.dart';
-import 'package:king_investor/presentation/pages/home/home_page.dart';
-import 'package:king_investor/presentation/pages/login/login_page.dart';
-import 'package:king_investor/presentation/pages/search/search_page.dart';
-import 'package:king_investor/presentation/pages/sign_up/sign_up_page.dart';
-import 'package:king_investor/presentation/pages/splash/splash_page.dart';
-import 'package:king_investor/presentation/pages/user/user_page.dart';
 import 'package:king_investor/presentation/static/app_routes.dart';
 
 void main() async {
@@ -27,7 +20,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Color(0x212429),
         backgroundColor: Color(0x212429),
         dialogBackgroundColor: Color(0x2A2D32).withAlpha(110),
-        cardColor: Colors.grey[900]?.withAlpha(180),
+        cardColor: Colors.grey[900]!.withAlpha(180),
         primaryColorDark: Colors.grey[900],
         canvasColor: Colors.grey[800],
         primaryColor: Colors.deepPurple,
@@ -42,35 +35,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       initialRoute: AppRoutes.splash,
-      getPages: [
-        GetPage(
-            name: AppRoutes.splash,
-            page: () => SplashPage(),
-            transition: Transition.zoom,
-            transitionDuration: Duration(milliseconds: 800)),
-        GetPage(
-            name: AppRoutes.home,
-            page: () => HomePage(),
-            transition: Transition.zoom,
-            transitionDuration: Duration(milliseconds: 800)),
-        GetPage(
-            name: AppRoutes.search,
-            page: () => SearchPage(),
-            transition: Transition.zoom,
-            transitionDuration: Duration(milliseconds: 400)),
-        GetPage(
-            name: AppRoutes.asset,
-            page: () => AssetPage(),
-            transition: Transition.zoom,
-            transitionDuration: Duration(milliseconds: 400)),
-        GetPage(
-            name: AppRoutes.user,
-            page: () => UserPage(),
-            transition: Transition.zoom,
-            transitionDuration: Duration(milliseconds: 400)),
-        GetPage(name: AppRoutes.login, page: () => LoginPage(), transition: Transition.rightToLeftWithFade),
-        GetPage(name: AppRoutes.signUp, page: () => SignUpPage(), transition: Transition.rightToLeftWithFade),
-      ],
+      getPages: AppRoutes.getAll(),
     );
   }
 }
--- lib/presentation/static/app_routes.dart
+++ lib/presentation/static/app_routes.dart
@@ -1,3 +1,12 @@
+import 'package:get/route_manager.dart';
+import 'package:king_investor/presentation/pages/asset/asset_page.dart';
+import 'package:king_investor/presentation/pages/home/home_page.dart';
+import 'package:king_investor/presentation/pages/login/login_page.dart';
+import 'package:king_investor/presentation/pages/search/search_page.dart';
+import 'package:king_investor/presentation/pages/sign_up/sign_up_page.dart';
+import 'package:king_investor/presentation/pages/splash/splash_page.dart';
+import 'package:king_investor/presentation/pages/user/user_page.dart';
+
 class AppRoutes {
   AppRoutes._();
 
@@ -8,4 +17,18 @@ class AppRoutes {
   static const search = '/search';
   static const asset = '/asset';
   static const user = '/user';
+
+  static List<GetPage<dynamic>>? getAll() {
+    return [
+      GetPage(name: splash, page: () => SplashPage(), transition: Transition.zoom, transitionDuration: _time(800)),
+      GetPage(name: home, page: () => HomePage(), transition: Transition.zoom, transitionDuration: _time(800)),
+      GetPage(name: search, page: () => SearchPage(), transition: Transition.zoom, transitionDuration: _time(400)),
+      GetPage(name: asset, page: () => AssetPage(), transition: Transition.zoom, transitionDuration: _time(400)),
+      GetPage(name: user, page: () => UserPage(), transition: Transition.zoom, transitionDuration: _time(400)),
+      GetPage(name: login, page: () => LoginPage(), transition: Transition.rightToLeftWithFade),
+      GetPage(name: signUp, page: () => SignUpPage(), transition: Transition.rightToLeftWithFade),
+    ];
+  }
+
+  static Duration _time(int duration) => Duration(milliseconds: duration);
 }
